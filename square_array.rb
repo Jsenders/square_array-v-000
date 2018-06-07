39f68cf5d2607cef86c81bc29127446d7aa7ba7b
Joseph Senders <jasenders@gmail.com>
2018-06-07 16:39:56 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,6 @@ def square_array(array)
   # your code here
   array = [2,4,6]
   array.each do |number|
-    puts "The square root of #{number} is #{number **}"
+    puts "#{number} squared is equal to #{number **}"
   end
 end
