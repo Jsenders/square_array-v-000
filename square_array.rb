3eb611fc481ef462b0a77b67f1018cac204a14a9
Joseph Senders <jasenders@gmail.com>
2018-06-07 16:40:43 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,6 @@ def square_array(array)
   # your code here
   array = [2,4,6]
   array.each do |number|
-    puts "#{number} squared is equal to #{number **}"
+    puts "#{number} squared is equal to #{number}"
   end
 end
